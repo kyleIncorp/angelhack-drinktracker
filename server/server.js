ca5828d596df40b0e2564c69245ed30f3c085a09
Mitchell Jeffrey <mitchell@cosmospostman.local>
2011-12-04 10:38:25 -0800

    jade for the email template; send it with POST to /dailyDrinkReport

--- server/server.js
+++ server/server.js
@@ -5,17 +5,25 @@ if( ! process.version=="v0.6.4")
 var express = require('express'),
     app = express.createServer();
     path = require('path');
+    jade = require('jade');
 
 //Static HTML files
 app.configure(function(){
 	app.use(express.static(path.normalize(__dirname+"/../client") ));
+	app.use(express.bodyParser());
+	app.set('view engine', 'jade');
+	app.set('view options', {layout: false});
 });
 
 //Otherwise...
-app.get('/', function(req, res){
-	res.send("GoonBuggy: What what in yr cup?");	
+app.post('/dailyDrinkReport', function(req, res){
+	console.log(req.body);
+	//Neat! req.body.* contains everything we need
+	//jade template and sendgrid
+	email = jade.renderFile('views/email.jade', {}, function(err, html){
+		console.log(html);
+	});
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port);
