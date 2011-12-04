30c5cb4112d4eb25b71b3d944135fd2021ded6d2
Mitchell Jeffrey <mitchell@cosmospostman.local>
2011-12-03 21:57:23 -0800

    Now serving up static html from client/ with express.static'

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ console.log("Hello, we're using "+process.version+" at "+__dirname);
 if( ! process.version=="v0.6.4")
 	require.paths.unshift(__dirname+"/node_modules");
 
-var app = require('express').createServer();
+var express = require('express'),
+    app = express.createServer();
+    path = require('path');
 
 //Static HTML files
 app.configure(function(){
-	app.use(express.static(__dirname + '../client'));
+	app.use(express.static(path.normalize(__dirname+"/../client") ));
 });
 
+//Otherwise...
 app.get('/', function(req, res){
 	res.send("GoonBuggy: What what in yr cup?");	
 });
