68fe1d627781722d203f0bf65da70d8064f12a65
Mitchell Jeffrey <mitchell@cosmospostman.local>
2011-12-03 21:40:29 -0800

    Attempting to rig static server from express

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
-console.log("Hello, we're using "+process.version);
+console.log("Hello, we're using "+process.version+" at "+__dirname);
 if( ! process.version=="v0.6.4")
 	require.paths.unshift(__dirname+"/node_modules");
 
-
 var app = require('express').createServer();
 
+//Static HTML files
+app.configure(function(){
+	app.use(express.static(__dirname + '../client'));
+});
+
 app.get('/', function(req, res){
-	res.send("GoonBuggy: What what in y`r cup?");	
+	res.send("GoonBuggy: What what in yr cup?");	
 });
 
 
